1254fba3aa79275cbf668810bcf9cd4aecaa6f44
gokuljp78 <141501629+gokuljp78@users.noreply.github.com>
2023-08-05 20:19:46 +0530

    Delete front end directory

--- front end/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import taskReducer from './slices/taskslice'
-
-const store = configureStore({
-    reducer: {
-        tasks:taskReducer
-    }
-});
-export default store
