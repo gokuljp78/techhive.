9e623a7a75098ef44a633bebbc0d914a64f941ae
gokuljp78 <141501629+gokuljp78@users.noreply.github.com>
2023-08-05 19:44:43 +0530

    Add files via upload

---
+++ front end/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TaskAdd from './components/taskadd';
+import Tasklist from './components/tasklist';
+import Modal from './components/update';
+import Update from './components/update';
+import Inter from './components/interface';
+
+function App() {
+  return (
+    <>
+    <div 
+    >
+     
+     < Inter/>
+     <TaskAdd/> 
+      <Modal/>
+      <Update/>
+      </div>
+          </>
+  );
+}
+
+export default App;
